f8d64a33c3f0a049d31d41f544830d51291a87fd
Massimiliano Pippi <mpippi@gmail.com>
2023-03-22 11:40:43 +0100

    stub for index

--- index/index.go
+++ index/index.go
@@ -1,5 +1,51 @@
 package index
 
+import (
+	"errors"
+	"io/ioutil"
+	"log"
+
+	"gopkg.in/yaml.v3"
+)
+
+type PromptIndex map[string]*Prompt
+
+var data map[string]*Prompt
+
+func init() {
+	data, _ = readFiles("./prompts") // FIXME: get path from config
+}
+
 func GetPrompt(name string) (*Prompt, error) {
-	return &Prompt{Name: name}, nil
+	val, ok := data[name]
+	if ok {
+		return val, nil
+	}
+	return nil, errors.New("not found")
+}
+
+func readFiles(path string) (PromptIndex, error) {
+
+	files, err := ioutil.ReadDir(path)
+	if err != nil {
+		return nil, err
+	}
+
+	retval := PromptIndex{}
+
+	for _, file := range files {
+		var p *Prompt
+		yamlFile, err := ioutil.ReadFile(file.Name())
+		if err != nil {
+			log.Printf("Readfile error:  %v", err)
+		}
+		err = yaml.Unmarshal(yamlFile, p)
+		if err != nil {
+			log.Fatalf("Unmarshal: %v", err)
+		}
+
+		retval[p.Name] = p
+	}
+
+	return retval, nil
 }
