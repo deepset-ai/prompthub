d741888b7ff60829f30fee03eb89693e623e6136
Massimiliano Pippi <mpippi@gmail.com>
2023-03-22 12:25:18 +0100

    data model

--- index/prompt.go
+++ index/prompt.go
@@ -2,5 +2,10 @@ package index
 
 // Prompt data model.
 type Prompt struct {
-	Name string `json:"name"`
+	Name        string            `json:"name"`
+	Tags        []string          `json:"tags"`
+	Meta        map[string]string `json:"meta"`
+	Version     string            `json:"version"`
+	Text        string            `json:"prompt_text"`
+	Description string            `json:"description"`
 }
