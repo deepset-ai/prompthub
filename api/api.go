8e8d849e4d6abfa707efb2ba94b2cede6e5e6c89
Massimiliano Pippi <mpippi@gmail.com>
2023-03-23 18:22:17 +0100

    remove unused function

--- api/api.go
+++ api/api.go
@@ -70,16 +70,6 @@ func ListPrompts(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-// paginate is a stub, but very possible to implement middleware logic
-// to handle the request params for handling a paginated request.
-func paginate(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// just a stub.. some ideas are to look at URL query params for something like
-		// the page number, or the limit, and send a query cursor down the chain
-		next.ServeHTTP(w, r)
-	})
-}
-
 func NewPromptListResponse(prompts []*index.Prompt) []render.Renderer {
 	list := []render.Renderer{}
 	for _, prompt := range prompts {
