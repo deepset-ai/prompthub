515352b9f673f24356441b917ba0da1b05a3304c
Silvano Cerza <3314350+silvanocerza@users.noreply.github.com>
2023-05-22 15:50:27 +0200

    Fix API returning null fields if not set in prompt yaml (#14)

--- index/prompt.go
+++ index/prompt.go
@@ -3,8 +3,8 @@ package index
 // Prompt data model.
 type Prompt struct {
 	Name        string                 `json:"name"`
-	Tags        []string               `json:"tags"`
-	Meta        map[string]interface{} `json:"meta"`
+	Tags        []string               `json:"tags,omitempty"`
+	Meta        map[string]interface{} `json:"meta,omitempty"`
 	Version     string                 `json:"version"`
 	Text        string                 `json:"prompt_text" yaml:"prompt_text"`
 	Description string                 `json:"description"`
